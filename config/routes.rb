2f2c2eadd75a226c375f417f2ab7e133651f26bc
AhmedShab <ahmed.vuw@gmail.com>
2016-06-09 15:08:47 +1200

    added signup page (user can sign up now)

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,13 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
   root 'welcome#home'
   get 'about', to: 'welcome#about'
 
   resources :articles
+
+  get 'signup', to: 'users#new'
+  resources :users, excpet: [:new]
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
